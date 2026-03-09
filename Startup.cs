31eb8571ea471209f66355b392b2d6d09436f26c
Odeliya-tr <104099669+Odeliya-tr@users.noreply.github.com>
2026-03-09 18:43:50 +0200

    Completed task 7
    Added synchronous communication with CatalogService

--- Startup.cs
+++ Startup.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -7,41 +8,57 @@ using Microsoft.OpenApi.Models;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.Serializers;
-using CatalogService.Repositories;
+using MongoDB.Driver;
+using InventoryService.Entities;
+using InventoryService.Clients;
+using Play.Common.Repositories;
+using Play.Common.Settings;
 
-namespace CatalogService
+namespace InventoryService
 {
-    public class Startup
+    public record Startup(IConfiguration Configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
             BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
 
+            var mongoDbSettings = Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
+            var serviceSettings = Configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
+
+            var mongoClient = new MongoClient($"mongodb://{mongoDbSettings.Host}:{mongoDbSettings.Port}");
+
+            services.AddSingleton(serviceProvider =>
+            {
+                return mongoClient.GetDatabase(serviceSettings.ServiceName);
+            });
+
+            services.AddSingleton<IRepository<InventoryItem>>(serviceProvider =>
+            {
+                var database = serviceProvider.GetRequiredService<IMongoDatabase>();
+                return new MongoRepository<InventoryItem>(database, "inventoryitems");
+            });
+
+            services.AddHttpClient<CatalogClient>(client =>
+            {
+                client.BaseAddress = new Uri("https://localhost:5001/");
+            });
+
             services.AddControllers(option => option.SuppressAsyncSuffixInActionNames = false);
+
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "CatalogService", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "InventoryService", Version = "v1" });
             });
-            services.AddSingleton<IItemRepository, ItemsRepository>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CatalogService v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "InventoryService v1"));
             }
 
             app.UseHttpsRedirection();
